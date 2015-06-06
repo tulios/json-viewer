aa9237731855f1dc0dac78ad46120f7511cd499b
tulios <ornelas.tulio@gmail.com>
2015-06-06 18:13:53 -0300

    ported original default

--- extension/src/viewer-styles.js
+++ extension/src/viewer-styles.js
@@ -1,4 +1,5 @@
 require('codemirror/lib/codemirror.css');
 require('codemirror/addon/fold/foldgutter.css');
+require('../styles/default-theme.scss');
 require('../styles/viewer-custom.scss');
 require('../styles/editor-custom.scss');
