6204caafc585d80b737ccb713a6df4e39659cd2b
tulios <ornelas.tulio@gmail.com>
2015-06-09 20:36:59 -0300

    Replace the numbers with their original values to avoid convert numbers bigger than Number.MAX_VALUE and so on. This is a better solution than the proposed by the issue (close #37)

--- extension/src/json-viewer/content-extractor.js
+++ extension/src/json-viewer/content-extractor.js
@@ -8,10 +8,21 @@ function contentExtractor(pre) {
       var rawJsonText = pre.textContent;
       var jsonExtracted = extractJSON(rawJsonText);
 
-      // validate and decode json
+      //validate and decode json
       var decodedJson = JSON.stringify(JSON.parse(jsonExtracted));
-      var jsonText = rawJsonText.replace(jsonExtracted, jsonFormater(decodedJson));
 
+      // match any numbers (integers, floats, negatives)
+      var regex = /\s*:\s*(-?\d+\.?\d*)/g;
+
+      // Replace the numbers with their original values to avoid convert numbers
+      // bigger than Number.MAX_VALUE and so on
+      // https://github.com/tulios/json-viewer/issues/37
+      var matches = rawJsonText.match(regex);
+      decodedJson.match(regex).forEach(function(number, i) {
+        decodedJson = decodedJson.replace(number, matches[i]);
+      });
+
+      var jsonText = rawJsonText.replace(jsonExtracted, jsonFormater(decodedJson));
       resolve({jsonText: jsonText, jsonExtracted: decodedJson});
 
     } catch(e) {
