51a87be8f5a75e327840bdf1624cde87db34a8f3
tulios <ornelas.tulio@gmail.com>
2015-07-05 11:39:39 +0200

    updated @dperini url regex (#issue 23)

--- extension/src/json-viewer/url-pattern.js
+++ extension/src/json-viewer/url-pattern.js
@@ -1,4 +1,5 @@
-// https://gist.github.com/dperini/729294
+// ** With some modifications **
+//
 // Regular Expression for URL validation
 //
 // Author: Diego Perini
@@ -27,7 +28,32 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
-
+//
+// the regular expression composed & commented
+// could be easily tweaked for RFC compliance,
+// it was expressly modified to fit & satisfy
+// these test for an URL shortener:
+//
+//   http://mathiasbynens.be/demo/url-regex
+//
+// Notes on possible differences from a standard/generic validation:
+//
+// - utf-8 char class take in consideration the full Unicode range
+// - TLDs have been made mandatory so single names like "localhost" fails
+// - protocols have been restricted to ftp, http and https only as requested
+//
+// Changes:
+//
+// - IP address dotted notation validation, range: 1.0.0.0 - 223.255.255.255
+//   first and last IP address of each class is considered invalid
+//   (since they are broadcast/network addresses)
+//
+// - Added exclusion of private, reserved and/or local networks ranges
+//
+// - Made starting path slash optional (http://example.com?foo=bar)
+//
+// - Allow a dot (.) at the end of hostnames (http://example.com.)
+//
 module.exports = new RegExp(
   "^" +
     // protocol identifier
@@ -35,23 +61,24 @@ module.exports = new RegExp(
     // user:pass authentication
     "(?:\\S+(?::\\S*)?@)?" +
     "(?:" +
-    // -- removed private and local exclusion
-    //   // IP address exclusion
-    //   // private & local networks
-    //   "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
-    //   "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
-    //   "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
-    //   // IP address dotted notation octets
-    //   // excludes loopback network 0.0.0.0
-    //   // excludes reserved space >= 224.0.0.0
-    //   // excludes network & broacast addresses
-    //   // (first & last IP address of each class)
-    //   "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
-    //   "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
-    //   "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+      // -- removed private and local exclusion
+      // IP address exclusion
+      // private & local networks
+      // "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+      // "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+      // "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+      // IP address dotted notation octets
+      // excludes loopback network 0.0.0.0
+      // excludes reserved space >= 224.0.0.0
+      // excludes network & broacast addresses
+      // -- removed first & last IP address of each class
+      // (first & last IP address of each class)
+      // "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+      // "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+      // "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
     // "|" +
-    // --
-    // -- included 0-9 to host, domain and TLD identifier
+      // --
+      // -- included 0-9 to host, domain and TLD identifier
       // host name
       "(?:(?:[a-z0-9\\u00a1-\\uffff0-9]-*)*[a-z0-9\\u00a1-\\uffff0-9]+)" +
       // domain name
