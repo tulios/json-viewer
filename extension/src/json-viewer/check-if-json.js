6dc458c8bbfaa648221b66c18a4d2823a594fad7
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-06-17 15:22:27 +0200

    improved json check

--- extension/src/json-viewer/check-if-json.js
+++ extension/src/json-viewer/check-if-json.js
@@ -5,9 +5,18 @@ function getPreWithSource() {
   return null
 }
 
+function isJSON(jsonStr) {
+   var str = jsonStr;
+   if (!str || str.length === 0) return false
+   str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@')
+   str = str.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+   str = str.replace(/(?:^|:|,)(?:\s*\[)+/g, '')
+   return (/^[\],:{}\s]*$/).test(str)
+ }
+
 function checkIfJson(sucessCallback, element) {
   var pre = element || getPreWithSource();
-  if (pre !== null && pre !== undefined) {
+  if (pre !== null && pre !== undefined && isJSON(pre.textContent)) {
     sucessCallback(pre);
   }
 }
