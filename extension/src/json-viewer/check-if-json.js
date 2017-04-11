5826a0d7d1291ff1a349c3580343c46fd54fa8d7
Nikola Pejoski <pejoski@gmail.com>
2017-04-11 22:24:30 +0200

    Reorder the childNodes checks

--- extension/src/json-viewer/check-if-json.js
+++ extension/src/json-viewer/check-if-json.js
@@ -9,6 +9,10 @@ function allTextNodes(nodes) {
 function getPreWithSource() {
   var childNodes = document.body.childNodes;
 
+  if (childNodes.length === 0) {
+    null
+  }
+
   if (childNodes.length > 1 && allTextNodes(childNodes)) {
     if (process.env.NODE_ENV === 'development') {
       console.debug("[JSONViewer] Loaded from a multiple text nodes, normalizing");
@@ -17,26 +21,25 @@ function getPreWithSource() {
     document.body.normalize() // concatenates adjacent text nodes
   }
 
-  if (childNodes.length === 1) {
-    var childNode = childNodes[0];
-    var nodeName = childNode.nodeName
-    var textContent = childNode.textContent
+  var childNode = childNodes[0];
+  var nodeName = childNode.nodeName
+  var textContent = childNode.textContent
 
-    if (nodeName === "PRE") {
-      return childNode;
+  if (nodeName === "PRE") {
+    return childNode;
+  }
 
-    // if Content-Type is text/html
-    } else if (nodeName === "#text" && textContent.trim().length > 0) {
-      if (process.env.NODE_ENV === 'development') {
-        console.debug("[JSONViewer] Loaded from a text node, this might have returned content-type: text/html");
-      }
-
-      var pre = document.createElement("pre");
-      pre.textContent = textContent;
-      document.body.removeChild(childNode);
-      document.body.appendChild(pre);
-      return pre;
+  // if Content-Type is text/html
+  if (nodeName === "#text" && textContent.trim().length > 0) {
+    if (process.env.NODE_ENV === 'development') {
+      console.debug("[JSONViewer] Loaded from a text node, this might have returned content-type: text/html");
     }
+
+    var pre = document.createElement("pre");
+    pre.textContent = textContent;
+    document.body.removeChild(childNode);
+    document.body.appendChild(pre);
+    return pre;
   }
 
   return null
