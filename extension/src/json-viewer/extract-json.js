be28c0617f15898274fac3afb7b102cc338928db
李真禛 <wednesday.co@gmail.com>
2017-06-28 11:50:43 +0800

    Fix the regular expression at the beginning and end of raw jsonp

--- extension/src/json-viewer/extract-json.js
+++ extension/src/json-viewer/extract-json.js
@@ -2,7 +2,7 @@ function extractJSON(rawJson) {
   return rawJson
     .replace(/\s*while\((1|true)\)\s*;?/, '')
     .replace(/\s*for\(;;\)\s*;?/, '')
-    .replace(/^[^{\[].+\({/, '{')
+    .replace(/^[^{\[].+\(\s?{/, '{')
     .replace(/}\s?\);?\s*$/, '}');
 }
 
